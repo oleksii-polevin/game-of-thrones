50312ba7a1a10e966eb6cfd93328bc837f3323f0
Oleksii Polevin <pololex327@gmail.com>
2019-10-18 09:47:10 +0300

    section right centered, modified event listeners, added classes invisible and error

--- src/main.js
+++ src/main.js
@@ -1,135 +1,83 @@
 const email = document.getElementById('mail');
 const password = document.getElementById('password');
-const forma = document.getElementById('firstForm');
+const signUp = document.getElementById('signUp');
 const name = document.getElementById('name');
 const select = document.getElementById('select');
 const textarea = document.getElementById('textarea');
-const forma2 = document.getElementById('save');
-//firstly wrong fields highlighted when button clicked
-//second time when input area become inactive
-//finally wrong fields highlighted after each inputted symbol
-const attempts = {
-  email: 0,
-  password: 0,
-  select: 0,
-  name: 0,
-  textarea: 0
-}
-//contains all function necessary for validation
+const save = document.getElementById('save');
 const validationRules =
 {
   email: function(elem) {
-    const regex = /\w{2,16}\@\w{2,6}\.\w{2,4}/;
+    const regex = /^\w{2,16}\@\w{2,6}\.\w{2,4}$/;
     return regex.test(elem);
   },
   password:function(elem) {
-    //at least one lowercase letter, one uppercase,and one digit; length >=8
-    // const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
-    // return regex.test(elem);
-    //for testing
+
     return elem.length > 7;
   },
   name: function(elem) {
-    return elem.length > 2;
+    const regex = /\w{2,20}/;
+    return regex.test(elem);
   },
   select: function(elem) {
-    const compare = document.getElementsByTagName('option');
-    for(i=0; i < compare.length; i++) {
-      if(elem === compare[i].innerHTML) {
-        return true;
-      }
-    }
-    return false;
+    return elem === '' ? false : true;
   },
   textarea: function(elem) {
-    const minNumberOfSymbols = 15;
-    const maxNumberOfSymbols = 300;
-    return elem.length > minNumberOfSymbols && elem.length < maxNumberOfSymbols;
+    return elem === '' ? false : true;
   }
 }
 
-forma.addEventListener('submit', function(e) {
-  e.preventDefault();
-  if((validationRules.password(password.value)) &&
-  (validationRules.email(email.value))) {
+signUp.addEventListener('click', function(e) {
+  if(validationRules.email(email.value) &&
+  validationRules.password(password.value)) {
     showSecondForm();
-    toggleMessage('hide');
-  } else {
-    toggleMessage('show');
-    if(!validationRules.password(password.value)) {
-      attemptSwitcher(password, 'password');
-    }
-    if(!validationRules.email(email.value)) {
-      attemptSwitcher(email, 'email');
-    }
+  }
+  if(!validationRules.email(email.value)) {
+    showError(email);
+    pinListener('email', email);
+  }
+  if(!validationRules.password(password.value)) {
+    showError(password);
+    pinListener('password', password);
   }
 });
 
-function toggleMessage(flag) {
-  flag === 'show' ?
-  document.getElementById('errorMessage').style.color = 'red':
-  document.getElementById('errorMessage').style.color = '#202020';
-}
-
-forma2.addEventListener('click', function(e) {
-  e.preventDefault();
+save.addEventListener('click', function() {
   if(validationRules.name(name.value) &&
-  validationRules.select(select.value) &&
   validationRules.textarea(textarea.value)) {
     alert('info is correct');
-    toggleMessage('hide');
-  } else {
-    toggleMessage('show');
-    if(!validationRules.name(name.value)) {
-      attemptSwitcher(name, 'name');
-    }
-    if(!validationRules["select"](select.value)) {
-      attemptSwitcher(select, 'select');
-    }
-    if(!validationRules.textarea(textarea.value)) {
-      attemptSwitcher(textarea, 'textarea');
-    }
   }
-})
-function attemptSwitcher(element, name) {
-  let elemName;
-  for(let key in attempts) {
-    if(key.toString() === name) {
-      elemName = key;
-    }
+  if(!validationRules.name(name.value)) {
+    showError(name);
+    pinListener('name', name);
   }
-  if(attempts[elemName] === 0) {
-    firstValidation(element);
-  } else if(attempts[elemName] === 1) {
-    focusValidation(element, name, 'focusout');
-  } else {
-    focusValidation(element, name, 'keyup');
+
+  if(!validationRules.textarea(textarea.value)) {
+    showError(textarea);
+    pinListener('textarea', textarea);
   }
-  attempts[elemName]++;
+});
+
+function checker(name, element) {
+const valid =  validationRules[name](element.value);
+valid ? removeError(element) : showError(element);
 }
 
-function firstValidation(element) {
-  element.style.borderBottom = '3px solid gray';
-}
-
-function focusValidation(element, name, eventType) {
-  let elemName;
-  for(let key in validationRules) {
-    if(key.toString() === name) {
-      elemName = key;
-    }
-  }
-  const norm = '3px solid green';
-  const invalid = '3px solid red';
-  element.addEventListener(eventType, function () {
-    validationRules[elemName](element.value) ? element.style.borderBottom = norm
-    : element.style.borderBottom = invalid;
+function pinListener(name, element) {
+  element.addEventListener('keyup', function() {
+    checker(name, element);
   })
-}
+};
 
+function showError(element) {
+  element.classList.add('error');
+}
+function removeError(element) {
+  element.classList.remove('error');
+}
 function showSecondForm() {
   const first = document.getElementById('firstForm');
   const second = document.getElementById('secondForm');
-  first.style.display = "none";
-  second.style.display = 'flex';
+  first.classList.add('invisible');
+  second.classList.remove('invisible');
 }
