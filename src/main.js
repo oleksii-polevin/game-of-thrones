5b893499911e58ad769b0bf6d1eea6041adafe4a
Oleksii Polevin <pololex327@gmail.com>
2019-10-21 11:34:41 +0300

    dropdown stylised correctly, simplified slider&dropdown communication, js aligned according to code style

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ const signUp = document.getElementById('signUp');
 const name = document.getElementById('name');
 const textarea = document.getElementById('textarea');
 const form = document.getElementById('form');
-const validationRules =
+const select = document.getElementById('houses');
+const isValid =
 {
   email: function(elem) {
     const regex = /^\w{2,16}\@\w{2,6}\.\w{2,4}$/;
@@ -18,30 +19,29 @@ const validationRules =
     return regex.test(elem);
   },
   select: function() {
-    const x =$('.current').text();
+    const x = $('.current').text();
     return x.indexOf('Select') === -1;
   },
   textarea: function(elem) {
-    const minlength = 9;
-    return elem.length > minlength ? true : false;
+    return elem.length > 3;
   }
 }
 
 form.addEventListener('submit', function(event) {
   event.preventDefault();
-  if(validationRules.name(name.value) &&
-  validationRules.select() &&
-  validationRules.textarea(textarea.value)) {
+  if (isValid.name(name.value)
+  &&  isValid.select()
+  &&  isValid.textarea(textarea.value)) {
     alert('info is correct');
   }
-  if(!validationRules.name(name.value)) {
+  if (!isValid.name(name.value)) {
     showError(name);
   }
-  if(!validationRules.textarea(textarea.value)) {
+  if (!isValid.textarea(textarea.value)) {
     showError(textarea);
   }
-  if(!validationRules.select()) {
-    $('.current').addClass('error');
+  if (!isValid.select()) {
+    $('.nice-select').addClass('error');
   }
 });
 
@@ -49,21 +49,22 @@ email.addEventListener('blur', function() {
   checker('email', email)
   pinListener('email', email);
 });
+
 password.addEventListener('blur', function() {
   checker('password', password)
   pinListener('password', password);
 });
 
 signUp.addEventListener('click', function(e) {
-  if(validationRules.email(email.value) &&
-  validationRules.password(password.value)) {
+  if (isValid.email(email.value)
+  &&  isValid.password(password.value)) {
     showSecondForm();
   }
-  if(!validationRules.email(email.value)) {
+  if (!isValid.email(email.value)) {
     showError(email);
     pinListener('email', email);
   }
-  if(!validationRules.password(password.value)) {
+  if (!isValid.password(password.value)) {
     showError(password);
     pinListener('password', password);
   }
@@ -72,76 +73,76 @@ signUp.addEventListener('click', function(e) {
 pinListener('name', name);
 pinListener('textarea', textarea);
 
-function checker(name, element) {
-  const valid =  validationRules[name](element.value);
+checker = (name, element) => {
+  const valid =  isValid[name](element.value);
   valid ? removeError(element) : showError(element);
 }
 
-function pinListener(name, element) {
+function pinListener (name, element) {
   element.addEventListener('keyup', function() {
     checker(name, element);
   })
 };
 
-function showError(element) {
+showError = (element) => {
   element.classList.add('error');
 }
-function removeError(element) {
+
+removeError = (element) => {
   element.classList.remove('error');
 }
-function showSecondForm() {
+
+showSecondForm = () => {
   const first = document.getElementById('firstForm');
   const second = document.getElementById('secondForm');
   first.classList.add('invisible');
   second.classList.remove('invisible');
 }
+
 //holds number of house in particular order for displaying
-//by carousel slider
 const houses =
 {
-  "Select your Great House": 7,
+  'Select your Great House': 'notSelected',
   arryn: 0,
-  baratheon:1,
+  baratheon: 1,
   greyjoy: 2,
   lannister: 3,
   martell: 4,
   stark: 5,
   tully: 6
 }
+
 //slider
 const owl = $('.owl-carousel');
-owl.owlCarousel({
-    items:1,
-    loop:true,
-    margin:10,
-    autoplay:true,
-    dots: false,
-    autoplayTimeout:2000,
-    autoplayHoverPause:true
-});
-$(document).ready(function() {
-  $('#houses').niceSelect();
-})
 
-$('#houses').on('change',function() {
+owl.owlCarousel({
+  items:1,
+  loop:true,
+  margin:10,
+  autoplay:false,
+  center: true,
+  dots: false,
+  autoplayTimeout:2000,
+  autoplayHoverPause:true,
+});
+
+//dropdown
+$('#houses').niceSelect();
+
+$('#houses').on('change', function() {
   const currentHouse = $('.current').text();
-  // console.log(currentHouse);
-  let number;
-  //dropdown wipe out everything except innerHTML,
-  //thus number of house (which is necesssary to display correct house)
-  //can be obtained from following loop
-  for(let key in houses) {
-     if(key === currentHouse){
-       number = houses[key];
-     }
-  }
-  if(number === 7) { //default
+  /*dropdown wipes out all except innerHTML
+  thus, in order to display correct picture
+  its number nedded
+  */
+  const houseNumber = houses[currentHouse];
+
+  if(houseNumber === 'notSelected') { //default
     owl.trigger('play.owl.autoplay', [2000]);
-    $('.current').addClass('error');
-  }
-  else {
+    $('.nice-select').addClass('error');
+  } else {
     owl.trigger('stop.owl.autoplay');
-    owl.trigger('to.owl.carousel', number);
-    $('.current').removeClass('error');
+    owl.trigger('to.owl.carousel', houseNumber);
+    $('.nice-select').removeClass('error');
   }
 });
